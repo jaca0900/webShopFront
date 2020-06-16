eb7000ae64673178c54aea9fd1b245c87506deff
Jacek Bednarczyk <jaca09001@gmail.com>
2020-06-16 17:05:58 +0200

    added basic payment with credit car

--- src/app/modules/items/components/cart/cart.component.ts
+++ src/app/modules/items/components/cart/cart.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../../../shared/services/storage/storage.service';
 import { IItem } from '../../models/item.interface';
 import { Router } from '@angular/router';
-import { CONSTANTS } from '../../../shared/constants';
 
 @Component({
   selector: 'app-cart',
@@ -17,6 +16,7 @@ export class CartComponent implements OnInit {
   postal: string;
   city: string;
   accounts: { number: string, price: number }[];
+  handler: any = null;
 
   constructor(private router: Router) {}
 
@@ -30,6 +30,8 @@ export class CartComponent implements OnInit {
     this.address = '';
     this.postal = '';
     this.city = '';
+
+    this.loadStripe();
   }
 
   mapPayment() {
@@ -65,8 +67,53 @@ export class CartComponent implements OnInit {
   }
 
   acceptAndPay() {
-    alert('Payment processed');
-    StorageService.clearCart();
-    this.router.navigate([CONSTANTS.MAIN_ROUTES.SHOP]);
+    // alert('Payment processed');
+    // StorageService.clearCart();
+    // const chk = sha256.x2(`vqTJuNoVXJ74wqve6K1cb0UNF6FOpEQqdev771202${this.total}PLNTest`);
+    // window.open(`https://ssl.dotpay.pl/test_payment/?chk=${chk}&id=771202&amount=${this.total}&currency=PLN&description=Test`, 'blank');
+    // // this.router.navigate([CONSTANTS.MAIN_ROUTES.SHOP]);
+
+    const handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_51GuZGMAcJVqnEjan8zqK2ilsJM7VlBfPVJPmDxdamZgeXZPAQiKiV75P7zMpkHOgjmqTYUPDTvglTPflwZaxAnKd00UfVxCKso',
+      locale: 'pl',
+      currency: 'PLN',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log(token);
+        alert('Token Created!!');
+      }
+    });
+
+    handler.open({
+      name: 'Demo Site',
+      description: '2 widgets',
+      amount: this.total// * 100
+    });
+  }
+
+  loadStripe() {
+
+    if (!window.document.getElementById('stripe-script')) {
+      const s = window.document.createElement('script');
+      s.id = 'stripe-script';
+      s.type = 'text/javascript';
+      s.src = 'https://checkout.stripe.com/checkout.js';
+      s.onload = () => {
+
+        this.handler = (<any>window).StripeCheckout.configure({
+          key: 'pk_test_aeUUjYYcx4XNfKVW60pmHTtI',
+          locale: 'auto',
+          token: function (token: any) {
+            // You can access the token ID with `token.id`.
+            // Get the token ID to your server-side code for use.
+            console.log(token);
+            alert('Payment Success!!');
+          }
+        });
+      };
+
+      window.document.body.appendChild(s);
+    }
   }
 }
